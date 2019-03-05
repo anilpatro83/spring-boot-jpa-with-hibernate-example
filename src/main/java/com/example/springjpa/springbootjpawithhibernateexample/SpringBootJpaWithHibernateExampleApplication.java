be5b865d6440677d7a6af034b1c8d10be7bfa058
Patro, Anil <anilpatro83@gmail.com>
2019-03-05 23:15:08 +0530

    first commit

---
+++ src/main/java/com/example/springjpa/springbootjpawithhibernateexample/SpringBootJpaWithHibernateExampleApplication.java
@@ -0,0 +1,13 @@
+package com.example.springjpa.springbootjpawithhibernateexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootJpaWithHibernateExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootJpaWithHibernateExampleApplication.class, args);
+	}
+
+}
