facc0bf3a34a084f587ef8157faee8b62feb1498
Anil Patro <anilpatro83@gmail.com>
2024-03-24 18:00:01 +0530

    version2 changes

---
+++ src/main/resources/data.sql
@@ -0,0 +1,2 @@
+INSERT INTO person (name, age, address, telephone) VALUES ('Laurent', 22, 'Wellington', '984283728');
+INSERT INTO person (name, age, address, telephone) VALUES ('Laurent2', 22, 'Wellington', '8935794389');
